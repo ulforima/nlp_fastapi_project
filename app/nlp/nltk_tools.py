e05c339417a386c37710aed4e77c0be749b5cc81
Софья Егорова <sofaegorova@MacBook-Air-Sofa.local>
2026-01-17 19:28:45 +0300

    Fix nltk_tools import path

--- app/nlp/nltk_tools.py
+++ app/nlp/nltk_tools.py
@@ -1,30 +1,57 @@
 import nltk
-from nltk import word_tokenize, pos_tag, ne_chunk
-from nltk.stem import PorterStemmer, WordNetLemmatizer
 
-# один раз скачать
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.stem import PorterStemmer, SnowballStemmer, WordNetLemmatizer
+from nltk import pos_tag, ne_chunk
+
+
+# Загрузка
 nltk.download("punkt")
 nltk.download("averaged_perceptron_tagger")
 nltk.download("maxent_ne_chunker")
 nltk.download("words")
 nltk.download("wordnet")
 
+
 def process_text(text):
-    tokens = word_tokenize(text)
+    """
+    NLP-анализ текста по:
+    - токенизация
+    - стемминг
+    - лемматизация
+    - POS-tagging
+    - Named Entity Recognition
+    """
 
-    stemmer = PorterStemmer()
-    stems = [stemmer.stem(t) for t in tokens]
+    # 1. Токенизация по предложениям
+    sentences = sent_tokenize(text)
 
+    # 2. Токенизация по словам
+    word_tokens = word_tokenize(text)
+
+    # 3. Стемминг
+    porter = PorterStemmer()
+    snowball = SnowballStemmer("english")
+
+    porter_stems = [porter.stem(w) for w in word_tokens]
+    snowball_stems = [snowball.stem(w) for w in word_tokens]
+
+    # 4. Лемматизация
     lemmatizer = WordNetLemmatizer()
-    lemmas = [lemmatizer.lemmatize(t) for t in tokens]
+    lemmas = [lemmatizer.lemmatize(w) for w in word_tokens]
 
-    pos = pos_tag(tokens)
-    ner = ne_chunk(pos)
+    # 5. Part-of-Speech tagging
+    pos_tags = pos_tag(word_tokens)
+
+    # 6. Named Entity Recognition
+    ner_tree = ne_chunk(pos_tags)
 
     return {
-        "tokens": tokens,
-        "stems": stems,
+        "sentences": sentences,
+        "word_tokens": word_tokens,
+        "porter_stems": porter_stems,
+        "snowball_stems": snowball_stems,
         "lemmas": lemmas,
-        "pos_tags": pos,
-        "named_entities": str(ner)
+        "pos_tags": pos_tags,
+        "named_entities": str(ner_tree)
     }
